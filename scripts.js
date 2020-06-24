e643b62993359b72f9529d399eb9f0bafab645e2
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-24 14:59:19 -0400

    Build togglePlay to play or pause video

--- scripts.js
+++ scripts.js
@@ -5,15 +5,18 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip');
 const ranges = player.querySelectorAll('.player__slider');
 
-
-
 // build out functions
-
+function togglePlay() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
 
 
 // hook up the event listeners.
